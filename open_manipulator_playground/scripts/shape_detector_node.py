cf854c6892d5f2b2d70efd95f7357d9177645589
dayeol kang <kangdy1997@gmail.com>
2026-04-23 13:49:54 +0900

    fixed ci
    Signed-off-by: dayeol kang <kangdy1997@gmail.com>

--- open_manipulator_playground/scripts/shape_detector_node.py
+++ open_manipulator_playground/scripts/shape_detector_node.py
@@ -1,11 +1,42 @@
 #!/usr/bin/env python3
-# Copyright 2026 ROBOTIS CO., LTD.
+#
+# Copyright 2024 ROBOTIS CO., LTD.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Author: Wonho Yun, Sungho Woo
 
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import PoseArray, Pose
 import cv2
 import numpy as np
+# Copyright 2024 ROBOTIS CO., LTD.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Author: Wonho Yun, Sungho Woo
+
 import os
 
 class ShapeDetectorNode(Node):
